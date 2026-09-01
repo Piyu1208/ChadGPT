a02ba768fd90f37f1b46ed09dcebf5dca3f690dd
Piyush Sharma <piyu22551@gmail.com>
2026-09-01 23:45:32 +0530

    final UI and more.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,11 @@
-import { ModeToggle } from '@/components/ui/mode-toggle';
-import {UserButton} from '@clerk/nextjs';
+import React from 'react';
+import { startNewChat } from '@/features/home/actions/start-new-chat';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <ModeToggle />
-      <UserButton />
-    </div>
-    
-  );
+const page = async () => {
+  const conversationId = await startNewChat();
+
+  redirect(`/c/${conversationId}`);
 }
+
+export default page;
